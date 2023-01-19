d5dbfd570139ba88dae5e9a8485e515df956ea5d
Biplab Das <31479620+Biplab96Das@users.noreply.github.com>
2023-01-19 23:12:57 +0530

    Add files via upload

---
+++ 222cs2268_Q_3/222cs2268_Q_3_server.py.py
@@ -0,0 +1,39 @@
+import socket
+#Author : Biplab Das
+#Roll No : 222CS2268
+#Department : Information Security(Computer Science & Engineering)
+# Initialize Socket Instance
+sock = socket.socket()
+print ("Server created successfully.")
+
+# Defining port and host
+port = 8800
+host = ''
+
+# binding to the host and port
+sock.bind((host, port))
+
+# Accepts up to 10 connections
+sock.listen(10)
+print('Server is listening...')
+
+while True:
+    # Establish connection with the clients.
+    con, addr = sock.accept()
+    print('Connected with ', addr)
+
+    # Get data from the client
+    data = con.recv(1024)
+    print(data.decode())
+    # Read File in binary
+    file = open('server-file.txt', 'rb')
+    line = file.read(1024)
+    # Keep sending data to the client
+    while(line):
+        con.send(line)
+        line = file.read(1024)
+
+    file.close()
+    print('File has been transferred successfully.')
+
+    con.close()
---
+++ 222cs2268_Q_3/Readme.md
@@ -0,0 +1,10 @@
+Initially,client-file.txt and server-file.txt are empty.
+step 1) Whatever text contents(numbers,character,symbols)want to include into file you want to send,write that into
+        server-file.txt, save it.
+step 2) Run 222cs2268_Q_3_server.py using the command "python3 222cs2268_Q_3_server.py".
+step 3) Run 222cs2268_Q_3_client.py in another terminal.
+step 4) Open client-file.txt,you will see server file has updated client file.That is sent to client by server
+        We have also output the content of server-file on client socket.
+#Author : Biplab Das
+#Roll No : 222CS2268
+#Dept : CSE        
